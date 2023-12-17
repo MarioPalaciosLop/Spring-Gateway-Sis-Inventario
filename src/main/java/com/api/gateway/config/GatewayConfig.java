f1531d897ec99ca1cb8cc9039cc13f3ce960494b
MarioPalaciosLop <plopezmarioalfr@gmail.com>
2023-12-17 17:54:07 -0500

    actualizacion gateway

--- src/main/java/com/api/gateway/config/GatewayConfig.java
+++ src/main/java/com/api/gateway/config/GatewayConfig.java
@@ -8,12 +8,17 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 @Configuration
 public class GatewayConfig {
 
+	String url_mario = "https://6af8-38-25-4-47.ngrok-free.app";
+	
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
+                .route("dev_nancy", r -> r.path("/api/empleados/**")
+                        .uri("https://f880-132-184-129-91.ngrok.io"))
+                .route("dev_antonio", r -> r.path("/api/carritoCompra/**")
+                        .uri("https://ec9c-170-81-241-210.ngrok.io"))
                 .route("dev_mario", r -> r.path("/api/grupos-proveedores/**")
-                        .filters(f -> f.rewritePath("/api/grupos-proveedores/(?<segment>.*)", "/${segment}"))
-                        .uri("https://6af8-38-25-4-47.ngrok-free.app"))
+                        .uri(url_mario))
                 .build();
     }
 }
